a9d06328d8283c240d4a41eb10b2046081956ee1
aptalca <thedjinn@yahoo.com>
2015-12-23 01:38:18 -0500

    Create firstrun.sh

---
+++ firstrun.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $(cat /etc/timezone) != $TZ ]] ; then
+  echo "$TZ" > /etc/timezone
+  dpkg-reconfigure -f noninteractive tzdata
+sed -i -e "s#;date.timezone.*#date.timezone = ${TZ}#g" /etc/php5/fpm/php.ini
+sed -i -e "s#;date.timezone.*#date.timezone = ${TZ}#g" /etc/php5/cli/php.ini
+fi
+
+mkdir -p /config/nginx/site-confs /config/www /config/log/nginx /config/keys
+
+if [ ! -f "/config/nginx/nginx.conf" ]; then
+cp /defaults/nginx.conf /config/nginx/nginx.conf
+fi
+
+if [ ! -f "/config/nginx/nginx-fpm.conf" ]; then
+cp /defaults/nginx-fpm.conf /config/nginx/nginx-fpm.conf
+fi
+
+if [ ! -f "/config/nginx/site-confs/default" ]; then
+cp /defaults/default /config/nginx/site-confs/default
+fi
+if [[ $(find /config/www -type f | wc -l) -eq 0 ]]; then
+cp /defaults/index.html /config/www/index.html
+fi
+cp /config/nginx/nginx-fpm.conf /etc/php5/fpm/pool.d/www.conf
+chown -R abc:abc /config
