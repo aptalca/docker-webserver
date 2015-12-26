abf8da284584640afccafa099b523f0ac655e055
aptalca <thedjinn@yahoo.com>
2015-12-26 00:55:24 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -10,25 +10,32 @@ fi
 mkdir -p /config/nginx/site-confs /config/www /config/log/nginx /config/keys
 
 if [ ! -f "/config/nginx/nginx.conf" ]; then
-cp /defaults/nginx.conf /config/nginx/nginx.conf
+  cp /defaults/nginx.conf /config/nginx/nginx.conf
 fi
 
 if [ ! -f "/config/nginx/nginx-fpm.conf" ]; then
-cp /defaults/nginx-fpm.conf /config/nginx/nginx-fpm.conf
+  cp /defaults/nginx-fpm.conf /config/nginx/nginx-fpm.conf
 fi
 
 if [ ! -f "/config/nginx/site-confs/default" ]; then
-cp /defaults/default /config/nginx/site-confs/default
+  cp /defaults/default /config/nginx/site-confs/default
 fi
+
 if [[ $(find /config/www -type f | wc -l) -eq 0 ]]; then
-cp /defaults/index.html /config/www/index.html
+  cp /defaults/index.html /config/www/index.html
 fi
+
 cp /config/nginx/nginx-fpm.conf /etc/php5/fpm/pool.d/www.conf
 
-if [ ! -d "/config/letsencrypt" ]; then
 cd /config
-git clone https://github.com/letsencrypt/letsencrypt
-cd letsencrypt
-./letsencrypt-auto certonly --standalone --email "$EMAIL" --agree-tos -d "$URL"
 
+if [ ! -d "/config/letsencrypt" ]; then
+  git clone https://github.com/letsencrypt/letsencrypt
+fi
+
+cd letsencrypt
+git pull
+sudo service nginx stop
+./letsencrypt-auto certonly --standalone --email "$EMAIL" --agree-tos -d "$URL"
 chown -R nobody:users /config
+sudo service nginx start
