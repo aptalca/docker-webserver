289d7885625ce43e8a114f14c596138c31777080
aptalca <thedjinn@yahoo.com>
2015-12-27 15:03:36 -0500

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "cronjob running at "$(date)
-/config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email \"$EMAIL\" --agree-tos -d \"$URL\"
+/config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
