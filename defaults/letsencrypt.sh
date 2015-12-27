9c8b482e66df337083f9bc9a963e74f29496e6d9
aptalca <thedjinn@yahoo.com>
2015-12-27 15:12:01 -0500

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "cronjob running at "$(date)
+service nginx stop
 /config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
+service nginx start
