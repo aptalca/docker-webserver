ff93ff72ed3ca4d6aa735e2b22a13ca0e5f4b2c3
aptalca <thedjinn@yahoo.com>
2015-12-28 22:10:18 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -65,6 +65,18 @@ else
 fi
 
 cd /config/letsencrypt
-./letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
+if [ ! -f "/config/keys/fullchain.pem" ]; then
+  echo "Generating server certificate for the first time"
+  ./letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
+else
+  diff=$(( (`date +%s` - `stat -c "%Y" /config/keys/fullchain.pem`) / 86400 ))
+  if [[ $diff > 60 ]]; then
+    echo "Renewing certificate that is older than 60 days"
+    ./letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
+  else
+    echo "Existing certificate is still valid, skipping renewal."
+  fi
+fi
+
 chown -R nobody:users /config
 service nginx start
