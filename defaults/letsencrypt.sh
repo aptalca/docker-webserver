9443d052e2c5ec36c31de95402799257f27901fc
aptalca <thedjinn@yahoo.com>
2015-12-28 22:12:54 -0500

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 echo "cronjob running at "$(date)
 service nginx stop
-/config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
+if [ ! -f "/config/keys/fullchain.pem" ]; then
+  echo "Generating server certificate for the first time"
+  /config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
+else
+  diff=$(( (`date +%s` - `stat -c "%Y" /config/keys/fullchain.pem`) / 86400 ))
+  if [[ $diff > 60 ]]; then
+    echo "Renewing certificate that is older than 60 days"
+    /config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL"
+  else
+    echo "Existing certificate is still valid, skipping renewal."
+  fi
+fi
 service nginx start
