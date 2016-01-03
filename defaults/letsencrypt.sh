2b4ad2bd367873e3c3c066581b0ce48cb65c6dad
aptalca <thedjinn@yahoo.com>
2016-01-03 14:24:02 -0500

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -3,7 +3,7 @@ echo "cronjob running at "$(date)
 if [ ! -f "/config/keys/fullchain.pem" ]; then
   echo "Generating server certificate for the first time"
   service nginx stop
-  /config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL" "$SUBDOMAINS2"
+  /config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email "$EMAIL" --agree-tos -d "$URL""$SUBDOMAINS2"
   chown -R nobody:users /config
 else
   diff=$(( (`date +%s` - `stat -c "%Y" /config/keys/fullchain.pem`) / 86400 ))
