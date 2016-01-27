8a9d08ae3edfaf486eb62336de731adc57143a71
aptalca <thedjinn@yahoo.com>
2016-01-27 13:37:01 -0500

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -2,32 +2,32 @@
 echo "cronjob running at "$(date)
 if [ ! -f "/config/keys/fullchain.pem" ]; then
   echo "Generating server certificate for the first time"
-  service nginx stop
   if [ ! -z $SUBDOMAINS ]; then
     for job in $(echo $SUBDOMAINS | tr "," " "); do
       export SUBDOMAINS2="$SUBDOMAINS2 -d "$job"."$URL""
     done
-    /config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email $EMAIL --agree-tos -d $URL $SUBDOMAINS2
+    /config/letsencrypt/letsencrypt-auto certonly --webroot -w /config/www/ --email $EMAIL --agree-tos -d $URL $SUBDOMAINS2
   else
-    /config/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --email $EMAIL --agree-tos -d $URL
+    /config/letsencrypt/letsencrypt-auto certonly --webroot -w /config/www/ --email $EMAIL --agree-tos -d $URL
   fi
   chown -R nobody:users /config
 else
-  diff=$(( (`date +%s` - `stat -c "%Y" /config/keys/fullchain.pem`) / 86400 ))
-  if [[ $diff -gt 60 ]]; then
+  EXP=$(date -d "`openssl x509 -in /config/keys/fullchain.pem -text -noout|grep "Not After"|cut -c 25-`" +%s)
+  DATENOW=$(date -d "now" +%s)
+  DAYS_EXP=$(( ( $EXP - $DATENOW ) / 86400 ))
+  if [[ $DAYS_EXP -lt 30 ]]; then
     echo "Renewing certificate that is older than 60 days"
-    service nginx stop
     if [ ! -z $SUBDOMAINS ]; then
       for job in $(echo $SUBDOMAINS | tr "," " "); do
         export SUBDOMAINS2="$SUBDOMAINS2 -d "$job"."$URL""
       done
-      /config/letsencrypt/letsencrypt-auto certonly --renew-by-default --standalone --standalone-supported-challenges tls-sni-01 --email $EMAIL --agree-tos -d $URL $SUBDOMAINS2
+      /config/letsencrypt/letsencrypt-auto certonly --renew-by-default --webroot -w /config/www/ --email $EMAIL --agree-tos -d $URL $SUBDOMAINS2
     else
-      /config/letsencrypt/letsencrypt-auto certonly --renew-by-default --standalone --standalone-supported-challenges tls-sni-01 --email $EMAIL --agree-tos -d $URL
+      /config/letsencrypt/letsencrypt-auto certonly --renew-by-default --webroot -w /config/www/ --email $EMAIL --agree-tos -d $URL
     fi
     chown -R nobody:users /config
   else
-    echo "Existing certificate is still valid and is only $diff day(s) old; skipping renewal."
+    echo "Existing certificate is still valid for another $DAYS_EXP day(s); skipping renewal."
   fi
 fi
-service nginx start
+service nginx reload
