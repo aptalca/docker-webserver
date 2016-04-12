e0ac531fc34ec66b429b3e0ca4a20c6db7bf81d5
aptalca <thedjinn@yahoo.com>
2016-04-12 13:55:47 -0400

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -3,9 +3,9 @@ echo "<------------------------------------------------->"
 echo
 echo "<------------------------------------------------->"
 echo "cronjob running at "$(date)
+export HOME="/root"
 cd /defaults
 . domains.conf
-cd /config/letsencrypt
 echo "URL is" $URL
 echo "Subdomains are" $SUBDOMAINS
 echo "letting the script update itself; help info may be displayed, you can ignore that :-)"
@@ -27,7 +27,7 @@ fi
 echo "Temporarily stopping Nginx"
 service nginx stop
 echo "Generating/Renewing certificate"
-./letsencrypt-auto certonly --renew-by-default --standalone --standalone-supported-challenges tls-sni-01 --email $EMAIL --agree-tos -d $URL $SUBDOMAINS2
+./letsencrypt-auto certonly --renew-by-default --standalone --standalone-supported-challenges tls-sni-01 --rsa-key-size 4096 --email $EMAIL --agree-tos -d $URL $SUBDOMAINS2
 chown -R nobody:users /config
 echo "Restarting web server"
 service nginx start
