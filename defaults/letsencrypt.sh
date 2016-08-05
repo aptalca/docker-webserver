3ae78e2161ee2aeaa026e0f15813f532053f1f46
aptalca <thedjinn@yahoo.com>
2016-08-05 18:52:11 -0400

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -6,4 +6,4 @@ echo "cronjob running at "$(date)
 export HOME="/root"
 cd /defaults
 echo "Running certbot renew"
-./certbot-auto -n renew --standalone --pre-hook "service nginx stop" --post-hook "service nginx start"
+./certbot-auto -n renew --standalone --pre-hook "service nginx stop" --post-hook "service nginx start ; cd /config/keys && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
