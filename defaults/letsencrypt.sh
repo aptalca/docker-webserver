cbffbcf45afdbed1b651078d14316e3835e75bf6
aptalca <thedjinn@yahoo.com>
2016-08-05 18:25:24 -0400

    Update letsencrypt.sh

--- defaults/letsencrypt.sh
+++ defaults/letsencrypt.sh
@@ -5,29 +5,5 @@ echo "<------------------------------------------------->"
 echo "cronjob running at "$(date)
 export HOME="/root"
 cd /defaults
-echo "Updating certbot script. It will display help info, which you can ignore"
-./certbot-auto -n -h
-. domains.conf
-echo "URL is" $URL
-echo "Subdomains are" $SUBDOMAINS
-echo "deciding whether to renew the cert(s)"
-if [ -f "/config/keys/fullchain.pem" ]; then
-  EXP=$(date -d "`openssl x509 -in /config/keys/fullchain.pem -text -noout|grep "Not After"|cut -c 25-`" +%s)
-  DATENOW=$(date -d "now" +%s)
-  DAYS_EXP=$(( ( $EXP - $DATENOW ) / 86400 ))
-  if [[ $DAYS_EXP -gt 30 ]]; then
-    echo "Existing certificate is still valid for another $DAYS_EXP day(s); skipping renewal."
-    exit 0
-  else
-    echo "Preparing to renew certificate that is older than 60 days"
-  fi
-else
-  echo "Preparing to generate server certificate for the first time"
-fi
-echo "Temporarily stopping Nginx"
-service nginx stop
-echo "Generating/Renewing certificate"
-./certbot-auto certonly --non-interactive --renew-by-default --standalone --standalone-supported-challenges tls-sni-01 --rsa-key-size 4096 --email $EMAIL --agree-tos -d $URL $SUBDOMAINS2
-chown -R nobody:users /config
-echo "Restarting web server"
-service nginx start
+echo "Running certbot renew"
+./certbot-auto -n renew --standalone --pre-hook "service nginx stop" --post-hook "service nginx start"
